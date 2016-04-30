a58e8682b0c19892a618e19cefbc54dae2871a30
Alex Lau <429095816@qq.com>
2016-04-30 10:28:22 +0800

    foundation

---
+++ NSMutableSet功能/NSMutableSet功能/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSMutableSet功能
+//
+//  Created by lwb on 16/4/28.
+//  Copyright © 2016年 lwb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+NSString *NscollectionToString(id collection)
+{
+    
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSMutableSet *set = [NSMutableSet setWithCapacity:10];
+        [set addObject:@"Jack"];
+        NSLog(@"Add one element: %@",)
+    }
+    return 0;
+}
